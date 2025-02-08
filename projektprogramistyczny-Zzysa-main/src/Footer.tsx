80fc6dc40cf55287cb813763e27453bd6bf3dfd4
Stanislau Liatsko <99603951+Zzysa@users.noreply.github.com>
2025-02-08 01:17:10 +0100

    Delete projektprogramistyczny-Zzysa-main directory

--- projektprogramistyczny-Zzysa-main/src/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer id="footer">
-            <p id="footerText">&copy; {new Date().getFullYear()} WebShop</p>
-        </footer>
-    );
-}
-
-export default Footer
